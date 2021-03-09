6594133b4376dc23c13597d94c4ef47411ae9f92
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-09 15:18:12 +0100

    Funzione per generare i 16 numeri random

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
+//Array contente i 16 numeri casuali generati
+var numeriErrore = [];
 //Generare 16 numeri casuali da 1 a 100
+randomNumber(1, 16)
   //Controllare se nei numeri generati ci sono dei duplicati
   //Chiedere all'utente di inserire numeri uno alla volta - capire quante volte
   //L'utente non può inserire più volte lo stesso numero.
@@ -9,3 +12,18 @@
 //Stampare resoconto partita
   //Esito partita
   //Numero di volte che è stato inserito un numero giusto
+
+
+// FUNCTIONS
+
+// Funzione per generare numeri random da minimo a massimo
+function randomNumber(min, max) {
+  var numeroRandom = "";
+
+  for (var i = min; i <= max; i++) {
+    numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
+    console.log(numeroRandom);
+  }
+
+  return numeroRandom;
+}

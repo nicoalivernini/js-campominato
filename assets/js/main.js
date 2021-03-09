c543aca29cab98b50ab5a9682f46479229b53d69
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-09 15:39:50 +0100

    Definizione funzione numeri random

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 //Array contente i 16 numeri casuali generati
 var numeriErrore = [];
 //Generare 16 numeri casuali da 1 a 100
-randomNumber(1, 16)
+numeriErrore = randomNumber(1, 16);
+console.log(numeroRandom);
   //Controllare se nei numeri generati ci sono dei duplicati
+
+
+
+
   //Chiedere all'utente di inserire numeri uno alla volta - capire quante volte
   //L'utente non può inserire più volte lo stesso numero.
   //Controllare se il numero messo dall'utente è presente nella lista dei numeri generati
@@ -16,14 +21,14 @@ randomNumber(1, 16)
 
 // FUNCTIONS
 
-// Funzione per generare numeri random da minimo a massimo
-function randomNumber(min, max) {
+function creatorErrore(min, max) {
   var numeroRandom = "";
 
-  for (var i = min; i <= max; i++) {
-    numeroRandom = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(numeroRandom);
-  }
 
-  return numeroRandom;
+}
+
+// Funzione per generare numeri random da minimo a massimo
+
+function creatorNumberRandom(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }

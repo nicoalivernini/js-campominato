b62ebcf3f7b7ee81c5ba2aee2f9813538639833c
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-09 17:55:55 +0100

    Esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,16 @@
 //Definisco array
 var numeriErrore = [];
 var numeriGiusti = [];
+var maxTentativi = 84;
+
+//Inserisco i 16 numeri da non pescare nell'array definito
+numeriErrore = createErrore(numeriErrore);
+
+//Salvare il punteggio
+ var risultato = svolgimento(numeriErrore, numeriGiusti, 100);
+
+ console.log("HAI PERSO");
+ console.log("Hai totalizzato: " + risultato);
 
 //Funzione per generare i 16 numeri da non pescare
 function createErrore(dati) {
@@ -16,31 +26,22 @@ function createErrore(dati) {
   return dati;
 }
 
-//Inserisco i 16 numeri da non pescare nell'array definito
-numeriErrore = createErrore(numeriErrore);
-
-//Contare 84 (da 1 e 100(numeri totali) - 16(numeri mortali)
-function process(numeriErrore, numeriGiusti, 100) {
-  while (true) {
-
+//Contare 84 (da 1 e 100 (numeri totali) - 16 (numeri mortali)
+function svolgimento(numeriErrore, numeriGiusti, maxTentativi) {
+  while (numeriGiusti.length < maxTentativi) {
+    //Utente inserisce il numero
+    var numeroUtente = parseInt(prompt("Inserisci un numero:"));
+    //Controllo che sia un numero, che è incluso tra 1 e 100 e che non sia stato precedentemente usato
+    if (!isNaN(numeroUtente) && 1 <= numeroUtente && numeroUtente <= 100 && numeriGiusti.includes(numeroUtente) == false) {
+      if (numeriErrore.includes(numeroUtente) == false) {
+        numeriGiusti.push(numeroUtente);
+      } else {
+        return numeriGiusti.length;
+      }
+    }
   }
+  return numeriGiusti.length
 }
-
-
-
-//L'utente non può inserire più volte lo stesso numero.
-//Controllare se il numero messo dall'utente è presente nella lista dei numeri generati
-//Se presente la partita termina, se non è presente chiedere un altro numero
-
-//La partita termina quando si inserisce un numero vietato o si finiscono il numero massimo di numeri consentiti
-//Stampare resoconto partita
-  //Esito partita
-  //Numero di volte che è stato inserito un numero giusto
-
-
-
-
-// Funzioni generali
 
 // Funzione per generare numeri random da minimo a massimo
 function creatorNumberRandom(min, max) {

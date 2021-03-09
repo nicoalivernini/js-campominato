e27a8101995cd8f0e963511a1bcec72801f283ba
Nicolò Alivernini <alivernininicolo@gmail.com>
2021-03-09 15:08:43 +0100

    Creazione file di base + divisione dei problemi

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//Generare 16 numeri casuali da 1 a 100
+  //Controllare se nei numeri generati ci sono dei duplicati
+  //Chiedere all'utente di inserire numeri uno alla volta - capire quante volte
+  //L'utente non può inserire più volte lo stesso numero.
+  //Controllare se il numero messo dall'utente è presente nella lista dei numeri generati
+  //Se presente la partita termina, se non è presente chiedere un altro numero
+
+//La partita termina quando si inserisce un numero vietato o si finiscono il numero massimo di numeri consentiti
+//Stampare resoconto partita
+  //Esito partita
+  //Numero di volte che è stato inserito un numero giusto
